d6b6b78d62d6cd93252297411ba31f7a9ce735ae
Ridvan <cadiroglu34@gmail.com>
2023-03-20 12:48:43 -0500

    TestNG Classes

---
+++ src/test/java/class01/DependsOn.java
@@ -0,0 +1,19 @@
+package class01;
+
+import org.testng.annotations.Test;
+
+public class DependsOn {
+
+    @Test
+    public void  Login(){
+        System.out.println("6/0");
+    }
+
+    @Test(dependsOnMethods = {"Login"})
+    public  void DashBoardverification(){
+        System.out.println("after login i am verifying dashboard");
+
+
+    }
+
+}
---
+++ src/test/java/class01/TestNGGannotations.java
@@ -0,0 +1,36 @@
+package class01;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGGannotations {
+
+    @BeforeMethod
+    public void beforeMethods(){
+        System.out.println("i am before Method");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("i am after Method");
+    }
+
+    @Test
+    public void aFirstTestCase(){
+        System.out.println("i am first test case");
+
+    }
+    @Test
+    public void bSecondTestCase(){
+        System.out.println("i am second test case");
+    }
+
+    @Test
+    public void cThirdTestCase(){
+        System.out.println("i am thirdTest case");
+
+
+    }
+
+}
